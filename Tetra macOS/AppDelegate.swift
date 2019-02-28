41af48e5279b771d9490484d71d51b054d4b60ee
Joss Manger <joss.manger@langlandusa.com>
2019-02-28 14:55:58 -0500

    Initial Commit

---
+++ Tetra macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Tetra macOS
+//
+//  Created by Joss Manger on 2/28/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
